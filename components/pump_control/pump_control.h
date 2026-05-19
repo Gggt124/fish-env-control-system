a50fbdef5e490cbf552e0924b7c7043cbf527f0d
Gggt124 <copter2737@gmail.com>
2026-05-19 20:20:47 +0700

    feat(01-02): add pump control API

---
+++ components/pump_control/pump_control.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "driver/gpio.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    PUMP_CONTROL_RELAY_ACTIVE_HIGH = 0,
+    PUMP_CONTROL_RELAY_ACTIVE_LOW,
+} pump_control_relay_polarity_t;
+
+typedef enum {
+    PUMP_CONTROL_FLOAT_UNKNOWN = 0,
+    PUMP_CONTROL_FLOAT_OFF,
+    PUMP_CONTROL_FLOAT_ON,
+} pump_control_float_state_t;
+
+typedef enum {
+    PUMP_CONTROL_TIMER_NONE = 0,
+    PUMP_CONTROL_TIMER_1,
+    PUMP_CONTROL_TIMER_2,
+} pump_control_active_timer_t;
+
+typedef enum {
+    PUMP_CONTROL_PHASE_IDLE = 0,
+    PUMP_CONTROL_PHASE_ON,
+    PUMP_CONTROL_PHASE_OFF,
+} pump_control_timer_phase_t;
+
+typedef struct {
+    uint32_t on_sec;
+    uint32_t off_sec;
+} pump_control_timer_config_t;
+
+typedef struct {
+    gpio_num_t float_gpio;
+    bool float_active_low;
+    gpio_num_t relay_gpio;
+    pump_control_relay_polarity_t relay_polarity;
+    uint32_t debounce_ms;
+    pump_control_timer_config_t timer1;
+    pump_control_timer_config_t timer2;
+} pump_control_config_t;
+
+typedef struct {
+    bool initialized;
+    bool config_valid;
+    bool running;
+    bool initial_stabilizing;
+    bool relay_energized;
+    gpio_num_t float_gpio;
+    gpio_num_t relay_gpio;
+    pump_control_float_state_t float_state;
+    pump_control_active_timer_t active_timer;
+    pump_control_timer_phase_t phase;
+    uint32_t countdown_sec;
+} pump_control_status_t;
+
+pump_control_config_t pump_control_default_config(void);
+bool pump_control_init(const pump_control_config_t *config);
+bool pump_control_start(void);
+bool pump_control_stop(void);
+bool pump_control_get_status(pump_control_status_t *out);
+
+#ifdef __cplusplus
+}
+#endif
